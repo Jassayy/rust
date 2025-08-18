80b9a76196b4b4f6ce9098580068cd49bfd26781
Jassayy <jasgandhi2004@gmail.com>
2025-08-18 13:28:44 +0530

    variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+// //print hellow world
+// fn main() {
+//     println!("Hello, world!");
+// }
+
+//simple variables in rust
+fn main(){
+    let mut x: i32 = 1; //32 bit number .. 'i' means signed integer ...'u' for unsigned f for float etc etc
+   
+
+    for i in 0..100{
+            x = x + 100; //value of x is changing so add mut keyword while defining the var as it is mutable
+    }
+     println!("x: {}" , x);
 }
