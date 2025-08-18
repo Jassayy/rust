793976e52c3a176945445c1324c768de5de99bd8
Jassayy <jasgandhi2004@gmail.com>
2025-08-18 14:02:06 +0530

    strings

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 fn main(){
-    let mut is_male: bool = true;
+    // let _ax = "kajsdib"; //can change space at runtime
+    //therefore needs more memory from the ram at runtime
+    //therefore we need to ask ram for more memory
+    let greeting: String = String::from("hello world");
 
-    is_male = false;
+    println!("{}" ,greeting);
+     let char: Option<char> = greeting.chars().nth(1); //type pf this optionally a char
 
-    if is_male {
-        println!("You are male");
-    }
-    //mut for mutable variables
+     //error: `Option<char>` doesn't implement `std::fmt::Display`
+     // the trait `std::fmt::Display` is not implemented for `Option<char>`
+    print!("{}" , char.unwrap()); //to get nth char of string
+    //therfore we cant do this.. we hve to "match"
+    //unwrap here works but dont use it 
 }
