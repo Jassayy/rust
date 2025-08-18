362a81bc53d822bbf71b5ddb782f14ab87be79da
Jassayy <jasgandhi2004@gmail.com>
2025-08-18 13:30:08 +0530

    booleans

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,7 @@
-// //print hellow world
-// fn main() {
-//     println!("Hello, world!");
-// }
-
-//simple variables in rust
 fn main(){
-    let mut x: i32 = 1; //32 bit number .. 'i' means signed integer ...'u' for unsigned f for float etc etc
-   
+    let is_male: bool = true;
 
-    for i in 0..100{
-            x = x + 100; //value of x is changing so add mut keyword while defining the var as it is mutable
+    if is_male {
+        println!("You are male");
     }
-     println!("x: {}" , x);
 }
