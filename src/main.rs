e31d2a6d4d41cf18b882a680bfa2615f3260f202
Jassayy <jasgandhi2004@gmail.com>
2025-08-22 15:58:06 +0530

    references

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,13 @@
+//Borrowing and references
+//Rihana now says I’d like to be borrowed from time to time. I will still have a single owner, but I can still be borrowed by other variables temporarily. What rules do you think should apply to her?
+// She can be borrowed by multiple people that she’s friends with but does no hanky panky
+// If she does want to do hanky panky, she can only have 1 borrower that she does it with. She cant simultaneously be with other borrowers (even with no hanky panky)
+ 
+//reference
 fn main() {
-    let my_string = String::from("hello");
-    let my_string3 =  takes_ownership(my_string);
-    println!("{}", my_string3); // This line would cause a compile error because ownership has been moved.
+    let s1 = String::from("Hello");
+    let s2 = &s1; //s1 is borrowed here
+
+    println!("{}", s2);
+    println!("{}", s1);    // This is valid, The first pointer wasn't invalidated
 }
-
-fn takes_ownership(some_string: String) -> String {
-    println!("{}", some_string); // `some_string` now owns the data.
-    //can rihana go back to her previous boyfriend? yes just return
-    return some_string;
-}
-
-
-//in this example rihana moves from boyfriend 1 -> boyfriend 2 and as he is about to die
-//rihana moves to boyfriend 3
-//what did we learn? rihana is a whore
